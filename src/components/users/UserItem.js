db6cffae78ebbd9975475db0f3df2c730a9bb52e
Mchoffn <mchoffn@hotmail.com>
2021-08-16 16:38:16 +0200

    opdate fra jsx

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class userItem extends Component {
+    constructor() {
+        super()
+        console.log(123)
+    }
+
+    render() {
+        return (
+            <div>
+                UserItem
+            </div>
+        )
+    }
+}
+
+
+export default userItem
