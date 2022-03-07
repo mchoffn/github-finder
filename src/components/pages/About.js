0a5ed81029491097860af06f3e65db1dca7e35dd
Andreas <ah@asseco.dk>
2022-03-07 19:48:14 +0100

    ny opdatering

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react';
 const About = () => {
   return (
     <Fragment>
-      <h1>About tddddddhis app</h1>
+      <h1>About tddddddddddhis app</h1>
       <p>App to search for github users</p>
       <p>Version 1.0.0</p>
     </Fragment>
